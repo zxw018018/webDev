e1759d47f89a21370503c4efbaf0e3ed74f47bb4
Xinwei <zxw018018@hotmail.com>
2018-01-14 22:11:31 -0500

    npm

---
+++ JavaScript/MyShop/listProducts.js
@@ -0,0 +1,7 @@
+var faker = require("faker");
+
+for (var i = 0; i < 10; i++){
+	var randomProductName = faker.commerce.productName();
+	var randomPrice = faker.commerce.price();
+	console.log(randomProductName + " is " + randomPrice + "dollars.");
+}
---
+++ JavaScript/grader.js
@@ -0,0 +1,15 @@
+function average(arr){
+	var sum = 0;
+	for (var i = 0; i < arr.length; i++){
+		sum += arr[i];
+	}
+	return Math.round(sum/(arr.length));
+	
+	
+}
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores));
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log(average(scores2));
