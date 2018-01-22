ba79ebab11dab02dd942ed9b3aa57733db8bcb94
Xinwei <zxw018018@hotmail.com>
2018-01-21 22:51:42 -0500

    embed and reference

---
+++ Associations/embed.js
@@ -0,0 +1,83 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo");
+
+
+
+// post - title, content
+var postSchema = new mongoose.Schema({
+	title: String,
+	content: String
+});
+
+
+
+var Post = mongoose.model("Post", postSchema);
+
+//user - email, name
+var userSchema = new mongoose.Schema({
+	email: String,
+	name: String,
+	posts: [postSchema]
+});
+
+var User = mongoose.model("User", userSchema);
+
+
+
+
+// var newUser = new User({
+// 	email: "hermione@hogwarts.edu",
+// 	name: "Hermione Granger"
+// });
+
+// newUser.posts.push({
+// 	title: "How to bre polyjuice potion",
+// 	content: "Just kidding."
+// });
+
+
+// newUser.save(function(err, user){
+// 	if(err){
+// 		console.log(err);
+// 	} else {
+// 		console.log(user);
+// 	}
+// });
+
+// var newPost = new Post({
+// 	title: "Reflections on Apples",
+// 	content: "They are delicious"
+// });
+// newPost.save(function(err, post){
+// 	if(err){
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+// });
+
+User.findOne({name: "Hermione Granger"}, function(err, user){
+	if (err){
+		console.log(err);
+	} else {
+		user.posts.push({
+			title: "3 things I really hate",
+			content: "Voldemort. Voldemort. Voldemort"
+		});
+		user.save(function(err, user){
+			if (err){
+				console.log(err);
+			} else {
+				console.log(user);
+			}
+		})
+	}
+});
+
+
+
+
+
+
+
+
