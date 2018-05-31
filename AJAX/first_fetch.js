cc7438eb697b06a4210a6548934408718767a20a
Xinwei <zxw018018@hotmail.com>
2018-05-31 23:40:33 +0800

    now on react track

---
+++ AJAX/first_fetch.js
@@ -0,0 +1,15 @@
+var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+        name: 'blue',
+        login: 'bluecat',
+    })
+})
+.then(function(response){
+    //console.log(response);
+    return response.json();
+})
+    .then(function(data){
+        console.log(data.bpi.EUR.rate);
+    });
